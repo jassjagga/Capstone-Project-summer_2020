0bb2f6679db9376ca4399b418702b08ef6dddf38
root <root@beaglebone.localdomain>
2020-08-14 20:29:58 +0000

    temprory files

---
+++ Capstone-Project/Temporary_files/gsm/pysms.py
@@ -0,0 +1,26 @@
+from time import sleep
+import serial
+from curses import ascii
+
+##set serial
+ser = serial.Serial()
+
+##Set port connection to USB port GSM modem 
+ser.port = '/dev/ttyO4'
+
+## set older phones to a baudrate of 9600 and new phones and 3G modems to 115200
+ser.baudrate = 9600
+##ser.baudrate = 115200
+ser.timeout = 1
+ser.open()
+def sendsms(number,text):
+    ser.write('AT+CMGF=1\r\n')
+    sleep(2)
+    ser.write('AT+CMGS="%s"\r\n' % number)
+    sleep(2)
+    ser.write('%s' % text)
+    sleep(2)
+    ser.write(ascii.ctrl('z'))
+
+
+sendsms('+16478072357','Alert! Patient Need attention')
